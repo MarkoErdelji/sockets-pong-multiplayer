27197f04e15150f386d4b1fe2e275fa80060d690
MarkoErdelji <erdeljimarko@gmail.com>
2022-09-24 16:39:49 +0200

    feat: Added rooms for multiple games at the same time

--- sockets.js
+++ sockets.js
@@ -3,27 +3,33 @@ function listen(io){
 io.on('connection',(socket)=>{
     console.log('a user connected', socket.id);
 
+    let room = 'room' + Math.floor(readyPlayerCount / 2);
+
     socket.on('ready', () => {
-        console.log('Player ready', socket.id);
+        socket.join(room);
+
+        console.log('Player ready', socket.id, room);
+
         readyPlayerCount++;
-        console.log(readyPlayerCount);
+
 
         if (readyPlayerCount % 2 === 0){
             console.log('game is ready to start!');
-            io.emit('startGame', socket.id)
+            io.in(room).emit('startGame', socket.id);
         }
     })
 
     socket.on('paddleMove',(paddleData)=>{
-        socket.broadcast.emit('paddleMove',paddleData);
+        socket.to(room).emit('paddleMove',paddleData);
     });
 
     socket.on('ballMove', (ballData) =>{
-        socket.broadcast.emit('ballMove',ballData);
+        socket.to(room).emit('ballMove',ballData);
     })
 
     socket.on('disconnect', (reason) =>{
         console.log(`Client ${socket.id} disconnected: ${reason}`);
+        socket.leave(room);
     });
 
 })
